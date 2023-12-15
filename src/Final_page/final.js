42b016dee4b94f6d923b38af9207a0adfce5a33d
RaghulR238 <raghulrajan2887@gmail.com>
2023-12-15 23:04:41 +0530

    day4.2

--- src/Final_page/final.js
+++ src/Final_page/final.js
@@ -12,47 +12,55 @@ import {useImage} from 'react-image';
 
 
 export default function Final() {
-    // const { src } = useImage({
-    //   srcList: imageUrl,
-    // });
+    
     
 const [title,setTitle]=useState([]);
-const [t,setT]=useState('');
+const [nutrient,setn]=useState();
+
 //const apiKey='4652d41224d74dbcb1ea92606a4e100f';
 //const apikeymaja='b3a6549e3e4142e6a9a8219944003f85';
 const apikeyVasi='75224b1718df478ea0892d31acae8ddf'
 
 
-const apiKey1='b9277005ebf74f12b62510043e2869a5';
+const apiKey1 = 'b9277005ebf74f12b62510043e2869a5';
+
 useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=burger&cuisine=italian&instructionsRequired=true&fillIngredients=true&addRecipeInformation=true&titleMatch=pasta&apiKey=${apiKey1}`);
-  
-        console.log(response.data);
-        console.log(response.data.results[0].title);
-  
-        setTitle(response.data.results[0]);
-      } catch (error) {
-        // Handle errors
-        console.error('Error fetching data:', error);
-      }
-    };
-  
-    fetchData();
-  }, []);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=burger&cuisine=italian&instructionsRequired=true&fillIngredients=true&addRecipeInformation=true&titleMatch=pasta&apiKey=${apiKey1}`);
+      
+      console.log(response.data);
+      console.log(response.data.results[0].title);
+
+      setTitle(response.data.results[0]);
+      const recipeId = response.data.results[0].id;
+
+      // Corrected the URL by adding '?' after 'nutritionLabel'
+      const nutrient = await axios.get(`https://api.spoonacular.com/recipes/${recipeId}/nutritionLabel?apiKey=${apiKey1}`);
+      
+      console.log(nutrient.data);
+      setn(nutrient.data);
+    } catch (error) {
+      // Handle errors
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  fetchData();
+}, []);
+
 
         return (
             <>
         {/* <Button onClick={fetchData}>Search</Button> */}
-      <Typography variant=''>
+      <Typography variant='h3'>
         Title:{title.title}
       </Typography>
             <Container>
         
         <CardMedia
         sx={{ height:'900px' }}
-        image={Image}
+        image={title.image}
         title="green iguana"
       />
       <Stack direction='column' >
@@ -80,13 +88,13 @@ useEffect(() => {
       <Grid container spacing={4}>
         {title.missedIngredients &&
           title.missedIngredients.map((card) => (
-            <Grid item key={card} xs={12} sm={6} md={4}>
-              <Card sx={{ height: '50%', display: 'flex', flexDirection: 'column' }}>
+            <Grid item key={card} xs={12} sm={6} md={4} >
+              <Card sx={{ height: '40%', display: 'flex', flexDirection: 'column' }}>
                 <CardMedia
                   component="div"
                   sx={{
-                    // 16:9
-                    //pt: '56.25%',
+                    marginLeft:"65px",
+                    marginTop:"10px",
                    width:'180px',
                    height:"180px"
                   }}
@@ -111,12 +119,32 @@ useEffect(() => {
         </Grid>
       </Stack>
       <Typography>
+        DIRECTIONS:
+      </Typography>
+      <Stack>
+  {title.analyzedInstructions &&
+    title.analyzedInstructions.map((item, index) => (
+      <div key={index}>
+        {item.steps.map((i, inIndex) => (
+          <ul key={inIndex}>
+            <li>{i.step}</li>
+          </ul>
+        ))}
+
+       
+      </div>
+    ))}
+</Stack>
+
+
+
+      <Typography>
   Summary:
   <div dangerouslySetInnerHTML={{ __html: title.summary }} />
 </Typography>
     <Stack>
 
-
+    <div dangerouslySetInnerHTML={{ __html: nutrient }}/>
     </Stack>
       
     </Container>
